18e9765a461789a1f8fdb6d065e5f236a6f0a377
suvikristiin <suvi.vehmaanpera@gmail.com>
2025-03-08 18:13:02 +0200

    add the answers to assignment 8

---
+++ week8/app/src/main/java/com/example/week8/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.week8;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText euroInput;
+    private TextView resultText;
+    private Button USDButton;
+    private Button GDPButton;
+    private double euroInteger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        euroInput = findViewById(R.id.EuroInput);
+        resultText = findViewById(R.id.ResultText);
+        USDButton = findViewById(R.id.USDButton);
+        GDPButton = findViewById(R.id.GDPButton);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void getEuroAmount() {
+        String euroString = euroInput.getText().toString();
+        euroInteger = Double.parseDouble(euroString);
+
+    }
+
+    public void euroToDollar(View view) {
+        getEuroAmount();
+        String dollar = String.format("%.2f", euroInteger * 1.05);
+        resultText.setText("$" + dollar);
+    }
+
+    public void euroToPound(View view) {
+        getEuroAmount();
+        String pound = String.format("%.2f", euroInteger * 0.83);
+        resultText.setText("£" + pound);
+    }
+}
